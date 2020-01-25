091049903f9216f743634ec517281b30863d1406
mm167 <32514329+mm167@users.noreply.github.com>
2020-01-25 23:57:57 +0100

    Update ApplicationTests.java

--- src/test/java/com/formation/ApplicationTests.java
+++ src/test/java/com/formation/ApplicationTests.java
@@ -15,10 +15,4 @@ class ApplicationTests {
     public void testSum() {
          assertEquals(5, calculator.sum(2, 3));
     }
-    
-    @Test
-    public void testNameMkyong() {
-        MessageBuilder obj = new MessageBuilder();
-        assertEquals("Hello Jean", obj.getMessage("Jean"));
-    }
 }
